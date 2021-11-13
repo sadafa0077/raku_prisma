db6bb43a93110db042d0182678ff7e219c460baf
sadafa0077 <densi007kw@gmail.com>
2021-11-13 20:49:22 +0900

    3kaime

--- script.ts
+++ script.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import { PrismaClient } from "@prisma/client";
+import { copyFileSync } from 'fs';
 
 const app = express()
 app.use(express.json());
@@ -14,13 +15,65 @@ app.post("/", async (req: Request, res: Response) => {
             password: password,
         },
     });
+    res.json(user);
 });
 
-app.get("/", async (req: Request, res: Response) => { });
+app.post("/createManyUsers", async (req: Request, res: Response) => {
+    const { userList } = req.body;
+    const users = await prisma.user.createMany({
+        data: userList,
+    })
+    res.json(users);
+});
 
-app.put("/", (req: Request, res: Response) => { });
+app.post("/createManyCars", async (req: Request, res: Response) => {
+    const { carList } = req.body;
+    const cars = await prisma.user.createMany({
+        data: carList,
+    })
+    res.json(cars);
+});
 
-app.delete("/:id", (req: Request, res: Response) => { });
+
+app.get("/", async (req: Request, res: Response) => {
+    const users = await prisma.user.findMany({
+        include: { cars: true },
+    });
+    res.json(users);
+});
+
+app.get("/byId/:id", async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const user = await prisma.user.findUnique({
+        where: {
+            id: Number(id),
+        },
+    });
+    res.json(user);
+});
+
+app.put("/", async (req: Request, res: Response) => {
+    const { id, username } = req.body
+    const updateUser = await prisma.user.update({
+        where: {
+            id: id
+        },
+        data: {
+            username: username,
+        },
+    });
+    res.json(updateUser)
+});
+
+app.delete("/:id", async (req: Request, res: Response) => {
+    const id = req.params.id
+    const deleteUser = await prisma.user.delete({
+        where: {
+            id: Number(id),
+        },
+    });
+    res.json(deleteUser);
+});
 
 
 app.listen(3001, () => {
